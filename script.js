3baa8729bc3f857b7fc261ff9acac0fc30b7f87d
Monica Li <monica.amanda.li@gmail.com>
2014-02-22 15:55:58 -0500

    Implement keylistener for ALT-t combination

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-$(document).ready(function () {
+var pressedKeys = {0: false, 1: false, 2: false, 3: false}; //alt, t, tab, enter
+var srchLinks = false;
 
+$(document).ready(function () {
+	window.addEventListener("keydown", downKey, true);
+	window.addEventListener("keyup", upKey, true);
 });
+
+function downKey(e) {
+	//alt key is pressed
+	if(e.keyCode == 18) {
+		pressedKeys[0] = true;
+	}
+	//t key is pressed
+	if(e.keyCode == 84) {
+		pressedKeys[1] = true;
+	}
+
+	var both = (pressedKeys[0] && pressedKeys[1]);
+	var neither = (!pressedKeys[0] && !pressedKeys[1]);
+	if(both || neither) {
+		srchLinks = !srchLinks;
+		pressedKeys[0] = !pressedKeys[0];
+		pressedKeys[1] = !pressedKeys[1];
+		
+
+		if(srchLinks) {
+			addUI();
+			alert("open things");
+		}
+		else {
+			removeUI();
+			alert("close things");
+		}
+	}
+}
+
+function keyUp(e) {
+	if(e.keyCode == 9) {
+		alert("move to next link");
+	}
+}
+
+addUI = function() {
+
+	$('.body').append(
+		'<div class="tabTargetInput">> <input type="text"> </input> </div>'
+		);
+}
+
+removeUI = function() {
+	$('.body').removeClass('tabTargetInput');
+}
