e98d5ad03b87d63740df144e03651b13c265022e
Maged Ahmed <magedma1@gmail.com>
2014-02-22 13:53:21 -0500

    basic link dictionary

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
-	console.log('Tab Target is running!');
 
+	console.log('Tab Target is running!');
+	var all_links = $('a');
+
+	var gict = [];
+	for (var i = 0; i < all_links.length; i ++ ) {
+		dict.
+
+	}
+
+	console.log('all_links: ' + all_links);
 });
