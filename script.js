b08e9a06b388f458a528d50b7453f8f4f34962f3
Maged Ahmed <magedma1@gmail.com>
2014-02-22 14:12:26 -0500

    initial dictionary syntax

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ $(document).ready(function () {
 	console.log('Tab Target is running!');
 	var all_links = $('a');
 
-	var gict = [];
+	var dict = [];
 	for (var i = 0; i < all_links.length; i ++ ) {
-		dict.
-
+		if (all_links[i].innerText.len > 0) {
+			dict.push({
+				key: all_links[i].innerText,
+				value: all_links[i]
+			});
+		}
 	}
 
 	console.log('all_links: ' + all_links);
