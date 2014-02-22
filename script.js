37557929c1b6271a0ea6fa9fb5c83a6a74e4b671
Jonathan Webb <jwebbed@outlook.com>
2014-02-22 14:09:32 -0500

    branch commit

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 $(document).ready(function () {
 
-	console.log('Tab Target is running!');
-	var all_links = $('a');
-
-	var gict = [];
-	for (var i = 0; i < all_links.length; i ++ ) {
-		dict.
-
-	}
-
-	console.log('all_links: ' + all_links);
 });
