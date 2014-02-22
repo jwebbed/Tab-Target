d590bc9702fa145123f1ec9e658f86d256e67d39
Maged Ahmed <magedma1@gmail.com>
2014-02-22 16:05:23 -0500

    basic clicking and color coding

--- script.js
+++ script.js
@@ -2,32 +2,44 @@ var dict = [];
 var all_links;
 
 $(document).ready(function () {
-
-	console.log('Tab Target is running!');
 	all_links = $('a');
-
 	
 	for (var i = 0; i < all_links.length; i ++ ) {
 		if (all_links[i].innerText.length > 0) {
 			dict.push({
-				key: all_links[i].innerText,
+				key: all_links[i].innerText.toLowerCase(),
 				value: all_links[i]
 			});
 		}
 	}
 
-	var matches = parseDict('Wikipedia');
+	var matches = parseDict('wiki');
+	var chosen_index = 0;
+	$(matches[chosen_index]).addClass('tt_chosen');
 
 });
 
 
 function parseDict(target_text) {
 	var matches = [];
-	for (var i = 0; i < all_links.length; i ++ ) {
-		if (all_links[i].innerText.indexOf(target_text) != -1) {
-			$(all_links[i]).highlight(target_text);
-			matches.push(all_links[i]);
+	target_text = target_text.toLowerCase();
+	for (var i = 0; i < dict.length; i ++ ) {
+		if (dict[i].key.indexOf(target_text) != -1) {
+			$(dict[i].value).highlight(target_text);
+			matches.push(dict[i].value);
 		}
 	}
 	return matches;
+}
+
+function switchChosen(curr_index, matches) {
+	$('.tt_chosen').removeClass('tt_chosen');
+	chosen_index += 1;
+	$(matches[chosen_index]).addClass('tt_chosen');
+	return chosen_index;
+}
+
+function clickLink() {
+	// click currently selected link
+	$('tt_chosen')[0].click();
 }
