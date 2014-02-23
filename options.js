e6b64fc8f69e756a0b8844d8542eda625fba50fa
Jonathan Webb <jwebbed@outlook.com>
2014-02-22 22:04:43 -0500

    yoyoyo

--- options.js
+++ options.js
@@ -1,49 +1,27 @@
 var storage = chrome.storage.sync;
 
-var cipher = document.querySelector("#typevals");
-$("#typevals").change(saveOptions);
-var keep = document.querySelector("#keep");
-$("#keep").change(saveOptions);
-
 var arr = [];
 
 loadSettings();
 
 function loadSettings() {
-  storage.get(['cipher', 'pass', 'keep'], function (items) {
-    if (items['cipher']) {
-      cipher.value = items['cipher'];
-    }
-    if (items['pass']) {
-      arr = items['pass'];
+  storage.get('sites', function (items) {
+    if (items['sites']) {
+      arr = items['sites'];
       for (i = 0; i < arr.length; i++) {
-        $("#saved").append("<li id='" + i + "'><span class='first'>" + arr[i][0] + "</span>" + arr[i][1]
+        $("#saved").append("<li id='" + i + "'><span class='first'>" + arr[i]
                             + "<div class='del'></div></li>");
         $('#' + i + ' .del').click(function () {
           $(this).parent().css("display", "none");
           var pos = arr.indexOf(i);
           if (~arr) arr.splice(pos, 1);
-          storage.set({'pass': arr});
+          storage.set({'sites': arr});
         });
       }
-    }
-    if (items['keep']) {
-      keep.checked = items['keep'];
     }
   });
 }
 
-function saveOptions() {
-  storage.set({'cipher': cipher.value });
-  storage.set({'keep': keep.checked });
-}
-
-$("#addpass").keydown(function (e) {
-  if (e.which == 13) {
-    $("#save").click();
-    $("#name").focus();
-  }
-});
 
 $("#save").click(function () {
   var site= $("#site").val();
@@ -54,8 +32,9 @@ $("#save").click(function () {
   }
 
   arr.push(site);
+
   $("#saved").append("<li><span class='first'>" + site + "</span></li>");
-  storage.set({'pass': arr});
+  storage.set({'sites': arr});
 
   $("#site").val("");
 });
